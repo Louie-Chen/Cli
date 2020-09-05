ded7ae8a8a49c8ff7f51096095ef3f085794cc93
Server <max0100618@gmail.com>
2020-09-06 00:49:10 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,6 @@ git add -A
 git commit -m 'deploy'
 
 # 部署到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:Louie-Chen/Cli.git master:gh-pages
+git push -f https://github.com/Louie-Chen/Cli.git master:gh-pages
 
 cd -
